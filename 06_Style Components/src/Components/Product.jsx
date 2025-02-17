d7b2bc77dc327fda5eaa4f735b4b7413ff19f2ae
Dhoni <ankitdhonee@gmail.com>
2025-02-17 18:13:11 +0545

    style components

---
+++ 06_Style Components/src/Components/Product.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Product.css'
+
+const Product = () => {
+  return (
+    <div className="product">
+      <h3>Product Title</h3>
+      <h5>Product Description</h5>
+    </div>
+  );
+};
+
+export default Product;
