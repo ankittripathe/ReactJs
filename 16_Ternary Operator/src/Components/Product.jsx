1bb7f083dbd9a18a8411645b28229b2d75a76e71
Dhoni <ankitdhonee@gmail.com>
2025-02-21 23:43:44 +0545

    Ternary operator

--- 16_Ternary Operator/src/Components/Product.jsx
+++ 16_Ternary Operator/src/Components/Product.jsx
@@ -8,9 +8,13 @@ const Product = ({ title, price }) => {
       <h3>Price: {price}</h3>
       {/* <h4>{price > 30000 ? '5%': ""}</h4> */}
       {/* {price > 30000 ? <p>"Discount of 5%"</p>: null} */}
-      {price > 30000 ? <p>"Discount of 5%"</p> : <a href="/">Get discount</a>}
+      {/* {price > 30000 ? <p>"Discount of 5%"</p> : <a href="/">Get discount</a>} */}
+      {price > 30000 && <p>"Discount of 5%"</p>}
     </div>
   );
 };
 
 export default Product;
+
+
+// && ke condition me agar condition true huwa tab jo print hogi nhi toh nhi hoga
