def614d405007795bbc90dbb54ea3752cdec2f2f
Dhoni <ankitdhonee@gmail.com>
2025-02-21 23:36:09 +0545

    ternary operator

---
+++ 16_Ternary Operator/src/Components/Product.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Product.css";
+
+const Product = ({ title, price }) => {
+  return (
+    <div className="product">
+      <h2>{title}</h2>
+      <h3>Price: {price}</h3>
+      {/* <h4>{price > 30000 ? '5%': ""}</h4> */}
+      {price > 30000 ? <p>"Discount of 5%"</p>: null}
+    </div>
+  );
+};
+
+export default Product;
